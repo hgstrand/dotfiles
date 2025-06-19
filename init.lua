2765b5c3a6017a7020614d71c1c544533034886e
Henrik S <henrik@gstrand.no>
2025-06-19 22:12:54 +0200

    add oil shortcut

--- init.lua
+++ init.lua
@@ -42,8 +42,16 @@ local plugins = {
       -- Optional dependencies
       dependencies = { { "echasnovski/mini.icons", opts = {} } },
       -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-}
-}
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+      },
+    }
+  }
 
 local opts = {}
 
@@ -56,10 +64,33 @@ vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 
 require("catppuccin").setup()
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+
 -- Setup telescope to show hidden files (dotfiles)
 require("telescope").setup({pickers={find_files={hidden=true}}})
 
 require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.cmd.colorscheme "catppuccin"
 
